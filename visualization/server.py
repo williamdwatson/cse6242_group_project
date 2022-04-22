126e6aea0778c18128dbe8450a1a3e9065023282
William Watson <williamdwatson1@gmail.com>
2022-04-22 00:57:30 -0500

    Server fix

--- visualization/server.py
+++ visualization/server.py
@@ -1,7 +1,7 @@
 import http.server, socketserver, webbrowser, os
 
 # Switch the working directory to the same directory as `server.py` (since that's where we want to serve from)
-os.chdir(os.path.dirname(__file__))
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 with socketserver.TCPServer(("", 8000), http.server.SimpleHTTPRequestHandler) as server:
     # Launch a new tab pointing to our landing page
     webbrowser.open_new_tab('http://localhost:8000/visualization.html')

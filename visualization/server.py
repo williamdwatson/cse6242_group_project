cdc8a04019a1e3d3facbc98e2516f7aba9c43982
William Watson <williamdwatson1@gmail.com>
2022-04-22 00:16:49 -0500

    Add Python launch

---
+++ visualization/server.py
@@ -0,0 +1,9 @@
+import http.server, socketserver, webbrowser, os
+
+# Switch the working directory to the same directory as `server.py` (since that's where we want to serve from)
+os.chdir(os.path.dirname(__file__))
+with socketserver.TCPServer(("", 8000), http.server.SimpleHTTPRequestHandler) as server:
+    # Launch a new tab pointing to our landing page
+    webbrowser.open_new_tab('http://localhost:8000/visualization.html')
+    # Run the server forever
+    server.serve_forever()
